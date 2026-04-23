eb5937c0f05be91ad04b3f491765be2bb57b185f
ViniciusGeraldoZamilian <zamilian.vinicius@escola.pr.gov.br>
2026-04-23 14:55:48 -0300

    lazario

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-const botoes = document.querySelectorAll(".botao");
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-04-25T00:00:00");
 
-for (let i = 0; i < botoes.length; i++) {
-  botoes[i].onclick = function () {
-    for (let j = 0; j < botoes.length; j++) {
-      botoes[j].classList.remove("ativo");
-    }
+function calculaTempo(tempoAlvo) {
+    const agora = new Date();
+    const diferenca = tempoAlvo - agora;
 
-    botoes[i].classList.add("ativo");
-  };
+    const dias = Math.floor(diferenca / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferenca / (1000 * 60 * 60)) % 24);
+    const minutos = Math.floor((diferenca / (1000 * 60)) % 60);
+    const segundos = Math.floor((diferenca / 1000) % 60);
+
+    return `${dias}d ${horas}h ${minutos}m ${segundos}s`;
 }
+
+function atualizaContador() {
+    contadores[0].textContent = calculaTempo(tempoObjetivo1);
+}
+
+// atualiza a cada segundo
+setInterval(atualizaContador, 1000);
